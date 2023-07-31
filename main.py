d3b2a1434486ff7a282d9c3e2e1ef61bd8ac957b
k-zanella <joaogabrielkondazanella@gmail.com>
2023-07-30 22:17:58 -0300

    Removido código e comentários

--- main.py
+++ main.py
@@ -2,54 +2,55 @@ import cv2
 from multiprocessing import Process, Queue
 from ultralytics import YOLO
 
-"""
-- There is a delay because the producer (frame collection from camera) 
-  is faster than the consumer (frame processing by model).
 
-- This can be resolved by lowering resolution or lowering queue max size.
-"""
+# There is a delay because the producer is faster than the consumer.
+# This can be resolved by lowering resolution or lowering queue max size.
+
+# Producer/Consumer Architecture:
+# - https://www.ni.com/en/support/documentation/supplemental/21/producer-consumer-architecture-in-labview0.html
 
 # Set supported camera resolution here
 # Must be multiple of 32 (320x320, 416x416, 448x448, 608x608, etc)
 RESOLUTION = (416, 416)
 
-# Define maxsize based in available memory and video delay
+# Define based in available memory and video delay (bigger size = more delay)
 QUEUE_SIZE = 20
 
-# Producer/Consumer Architecture:
-# - https://www.ni.com/en/support/documentation/supplemental/21/producer-consumer-architecture-in-labview0.html
 
-
-# TODO: function to generate new model based on current resolution
-# check if current model in models folder has imgsz of the current resolution
-# if not, generate new one and delete older
-
+# Generates frames from device camera
 def producer(queue, camera):
     try:
         while camera.isOpened():
             is_read, frame = camera.read()
             if is_read:
                 queue.put(frame)
-            print(f"Queue Size = {queue.qsize()}")
+            #print(f"Queue Size = {queue.qsize()}")
 
     finally:
         camera.release()
 
 
+# Uses frames as input to YOLO model and show on window
 def consumer(queue, model):
     try:
         while True:
             results = model.predict(source=queue.get(), imgsz=RESOLUTION[0], classes=0, device="cpu", verbose=False)
             cv2.imshow("Live People Detection", results[0].plot())
-
-            if cv2.waitKey(1) & 0xFF == ord("q"):  # press "q" to exit window
-                break
+            cv2.waitKey(1)  # cv2 doesn't work without this
 
     finally:
         cv2.destroyAllWindows()
 
 
+# Creates new ONNX model based on PyTorch (pt) model
+# If pt model is not found, it will automatically download it
+def export_new_model():
+    model = YOLO(model="models/yolov8n.pt", task="detect")
+    model.export(format="onnx", imgsz=RESOLUTION[0], classes=0, dynamic=False, simplify=True)
+
+
 if __name__ == "__main__":
+    #export_new_model()
     model = YOLO(model="models/yolov8n.onnx", task="detect")
 
     camera = cv2.VideoCapture(0)  # use default camera (index 0)
